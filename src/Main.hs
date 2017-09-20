cb9d7344546837b6f9e565a7c4014a79ee6e9017
Peter Andersson <pa222ku@student.lnu.se>
2017-09-20 16:38:44 +0000

    Make use of Error sum type

--- src/Main.hs
+++ src/Main.hs
@@ -10,11 +10,11 @@ import           Data.Aeson.Lens (AsValue, key, nth, _Number, _String)
 import qualified Data.ByteString.Char8 as S8
 import           Data.ByteString.Lazy.Char8 as L8 (ByteString)
 import           Data.Scientific (Scientific, coefficient, scientific)
-import           Data.Text (Text, unpack)
+import           Data.Text as T (Text, concat, pack, unpack)
 import           Lens.Micro ((^?))
 import           Network.HTTP.Client (httpLbs, newManager, responseBody, responseStatus, Response, Request, RequestBody (RequestBodyLBS))
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
-import           Network.HTTP.Conduit (defaultRequest, method, secure, host, path, requestBody, requestHeaders)
+import           Network.HTTP.Conduit (defaultRequest, host, method, path, requestBody, requestHeaders, secure)
 import           Network.HTTP.Types.Header (Header, hAuthorization, hContentType)
 import           Network.HTTP.Types.Status (statusCode)
 import           Prelude hiding (id)
@@ -24,12 +24,19 @@ import           Text.Read (reads)
 
 import           Network (authHeaders, destroyDropletRequest, snapshotsRequest, startDropletRequest)
 import           Token (getToken, tokenSanityCheck)
-import           Types (DropletId (DropletId), SnapshotId (SnapshotId), Token, getSecret, unDropletId, unSnapshotId)
+import           Types (DropletId (DropletId), Error (..), SnapshotId (SnapshotId), Token, getSecret, unDropletId, unSnapshotId)
 
-decodeBody :: Response L8.ByteString -> Either String Value
-decodeBody = eitherDecode' . responseBody
+mapError :: (a -> c) -> Either a b -> Either c b
+mapError f (Left x) = Left $ f x
+mapError _ (Right x) = Right x
 
-getSnapshotId :: AsValue s => s -> Maybe Text
+decodeBody :: Response L8.ByteString -> Either Error Value
+decodeBody = mapError toParseResponseError . eitherDecode' . responseBody
+
+toParseResponseError :: String -> Error
+toParseResponseError = ParseResponse . T.pack
+
+getSnapshotId :: AsValue s => s -> Maybe T.Text
 getSnapshotId x = x ^? key "snapshots" . nth 0 . key "id" . _String
 
 getDropletId :: AsValue s => s -> Maybe Scientific
@@ -38,23 +45,19 @@ getDropletId x = x ^? key "droplet" . key "id" . _Number
 maybeToEither :: e -> Maybe a -> Either e a
 maybeToEither e = maybe (Left e) Right
 
-parseText :: Text -> Either String Scientific
-parseText text = case (reads . Data.Text.unpack $ text :: [(Integer, String)]) of
-                  [] -> err
+parseText :: Text -> Either Error Scientific
+parseText text = case (reads . T.unpack $ text :: [(Integer, String)]) of
                   [(x, [])] -> Right (scientific x 0)
-                  _ -> err
+                  _ -> mapError ParseResponse err
                   where
-                    err = Left $ concat
-                      [ "Could not cast Text '"
-                      , Data.Text.unpack text
-                      , "' to Scientific" ]
+                    err = Left $ T.concat ["Could not cast Text '", text, "' to Scientific"]
 
+parseSnapshotId :: Response ByteString -> Either Error Text
+parseSnapshotId = maybeToEither ParseSnapshotId . getSnapshotId . responseBody
 
-parseSnapshotId :: Response ByteString -> Either String Text
-parseSnapshotId = maybeToEither "Could not parse snapshot id" . getSnapshotId . responseBody
-
-parseDropletId :: Response ByteString -> Either String DropletId
-parseDropletId = fmap DropletId . maybeToEither "Could not parse droplet id" . getDropletId . responseBody
+parseDropletId :: Response ByteString -> Either Error DropletId
+parseDropletId =
+  fmap DropletId . maybeToEither ParseDropletId . getDropletId . responseBody
 
 requestObject :: SnapshotId -> Value
 requestObject id = object
@@ -66,44 +69,47 @@ requestObject id = object
 snapshotRequest :: Token -> SnapshotId -> Request
 snapshotRequest token = startDropletRequest token . RequestBodyLBS . encode . requestObject
 
-startSnapshotIO :: Token -> SnapshotId -> IO (Either String DropletId)
+startSnapshotIO :: Token -> SnapshotId -> IO (Either Error DropletId)
 startSnapshotIO token id = do
   manager <- newManager tlsManagerSettings
   response <- httpLbs (snapshotRequest token id) manager
-  return . fmap DropletId . maybeToEither "Could not parse droplet id" $ getDropletId $ responseBody response
+  return . fmap DropletId . maybeToEither ParseDropletId $ getDropletId $
+    responseBody response
 
-getSnapshotIO :: Token -> IO (Either String SnapshotId)
+getSnapshotIO :: Token -> IO (Either Error SnapshotId)
 getSnapshotIO token = do
   manager <- newManager tlsManagerSettings
   response <- httpLbs (snapshotsRequest token) manager
   return $ fmap SnapshotId (parseSnapshotId response >>= parseText)
 
-destroyDropletIO :: Token -> DropletId -> IO (Either String DropletId)
+destroyDropletIO :: Token -> DropletId -> IO (Either Error DropletId)
 destroyDropletIO token id = do
   manager <- newManager tlsManagerSettings
   response <- httpLbs (destroyDropletRequest token id) manager
   case statusCode (responseStatus response) of
     204 -> return $ Right id
-    n -> return $ Left $ "Response status code: " ++ show n
+    n ->
+      return $ mapError (DropletIdNotFound) $ Left $
+      T.concat ["Response status code: ", T.pack . show $ n]
 
-getTokenIO :: IO (Either String Token)
+getTokenIO :: IO (Either Error Token)
 getTokenIO = do
   args <- getArgs
-  return (getToken args >>= tokenSanityCheck)
+  return $ mapError (const NoToken) $ (getToken args >>= tokenSanityCheck)
 
-startDropletFromSnapshot :: EitherT String IO DropletId
+startDropletFromSnapshot :: EitherT Error IO DropletId
 startDropletFromSnapshot = do
   token <- EitherT getTokenIO
   liftIO $ putStrLn "Token received"
   snapshotId <- EitherT $ getSnapshotIO token
   EitherT $ startSnapshotIO token snapshotId
 
-destroyDroplet :: DropletId -> EitherT String IO DropletId
+destroyDroplet :: DropletId -> EitherT Error IO DropletId
 destroyDroplet id = do
   token <- EitherT getTokenIO
   EitherT $ destroyDropletIO token id
 
-wait :: DropletId -> EitherT String IO DropletId
+wait :: DropletId -> EitherT Error IO DropletId
 wait id = liftIO (threadDelay (200*1000*1000)) >> return id
 
 main :: IO ()
@@ -111,5 +117,5 @@ main = do
   dropletId <- runEitherT (startDropletFromSnapshot >>= wait >>= destroyDroplet)
   case dropletId of
     (Right id) -> putStrLn . (++) "Success: ". show . coefficient . unDropletId $ id
-    (Left err) -> putStrLn . (++) "Error: " $ err
+    (Left err) -> putStrLn . (++) "Error: " $ show err
   return ()
--- src/Types.hs
+++ src/Types.hs
@@ -5,13 +5,12 @@ import           Data.Scientific (Scientific, coefficient, scientific)
 
 newtype Token = Token { getSecret :: String }
 
-data Error = TokenLength Int | NoToken | CastTextToScientific Text | ParseSnapshotId | ParseDropletId | DropletIdNotFound Int
+data Error = TokenLength Int | NoToken | ParseResponse Text | ParseSnapshotId | ParseDropletId | DropletIdNotFound Text
 
 instance Show Error where
   show (TokenLength n) = "Token length is " ++ show n ++ "."
   show NoToken = "You need to supply at least one argument."
-  show (CastTextToScientific s) =
-    "Could not cast Text '" ++ Data.Text.unpack s ++ "' to Scientific"
+  show (ParseResponse s) = "Could not parse Text '" ++ Data.Text.unpack s ++ "' to Scientific"
   show ParseSnapshotId = "Could not parse snapshot id."
   show ParseDropletId = "Could not parse droplet id."
   show (DropletIdNotFound n) = "Response status code: " ++ show n
